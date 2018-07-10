348942bde4a2e9f0096017480fb41b6b1c4fe399
suniyun <suni.yun@hotmail.com>
2018-07-10 12:24:24 -0300

    Todos los ejercicios IF hechos

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,18 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var notas = Math.floor(Math.random()*(11-1))+1;
+
+	if(notas >= 9){
+		alert("Excelente");
+	}
+	else if (notas > 4){
+		alert("Aprobó");
+	}
+	else {
+		alert("Vamos, la proxima se puede");
+	}
+
 	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,10 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+
+	var rand = Math.floor(Math.random()*(11-1))+1;
+	alert(rand);
+	
 	
 
 }//FIN DE LA FUNCIÓN
